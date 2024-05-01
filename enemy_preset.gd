d4b9de30b73a048d2c50b2779db23ac7020e66d2
Pingu <sunha2300@gmail.com>
2024-05-01 17:24:55 +0900

    enemy 쏴서 두 대 맞추면 queue_free()
    enemy hp 100으로 설정. 맞으면 50씩 다는 걸로 설정. enemypreset에 코드 있음

--- enemy_preset.gd
+++ enemy_preset.gd
@@ -1,7 +1,19 @@
 extends CharacterBody2D
 
+const HP = 100
 const SPEED = 200.0
+var hp = HP
+
+func ready():
+	pass
 
 func _physics_process(delta):
 	velocity = (Player.position - position).normalized() * SPEED
 	move_and_slide()
+	if hp <= 0:
+		queue_free()
+
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("bullet") == true:
+		hp -= 50
--- game.gd
+++ game.gd
@@ -4,12 +4,12 @@ var spawn_margin = 1200
 var EnemyPresetScene = preload("res://enemy_preset.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	spawn_enemy()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	spawn_enemy()
+	pass
 
 func spawn_enemy():
 
