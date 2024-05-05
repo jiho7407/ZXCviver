0d06182f96cefac2726866e1dac39e575fe3f1ca
Pingu <sunha2300@gmail.com>
2024-05-05 22:06:31 +0900

    feat: 옵션 팝업 추가
    - title screen에서 옵션 버튼 누르면 작동하게 구현.
    - play screen 에서 옵션 버튼 동작하게 하는 건 title screen에서 완성 시킨 후 할 예정.

--- TitleScreen/TitleScreen.gd
+++ TitleScreen/TitleScreen.gd
@@ -14,6 +14,29 @@ func _on_start_pressed():
 	get_tree().change_scene_to_file("res://PlayScreen/play_screen.tscn")
 
 
+func _on_options_pressed():
+	$CanvasLayer/OptionsWindow.show()
+
+
+func _on_options_window_close_requested():
+	$CanvasLayer/OptionsWindow.hide()
+
+
+
 func _on_exit_pressed():
 	await get_tree().create_timer(0.1).timeout
 	get_tree().quit()
+
+
+
+
+
+
+
+
+func _on_check_button_toggled(toggled_on):
+	if toggled_on == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	if toggled_on == false:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
