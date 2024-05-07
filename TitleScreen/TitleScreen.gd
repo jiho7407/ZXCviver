ade8dfadf3c2af0f2b4fa8c3d687b927e03423e3
Pingu <sunha2300@gmail.com>
2024-05-07 11:39:28 +0900

    feat: AudioServer Bus 추가
    -SFX 버스, BGM 버스 추가
    -옵션에서 슬라이더로 버스 음량 조절 기능 추가

--- TitleScreen/TitleScreen.gd
+++ TitleScreen/TitleScreen.gd
@@ -22,6 +22,13 @@ func _on_options_window_close_requested():
 	$CanvasLayer/OptionsWindow.hide()
 
 
+func _on_sounds_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db(value))
+
+
+func _on_music_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("BGM"), linear_to_db(value))
+
 
 func _on_exit_pressed():
 	await get_tree().create_timer(0.1).timeout
@@ -39,4 +46,5 @@ func _on_check_button_toggled(toggled_on):
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	if toggled_on == false:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
 
