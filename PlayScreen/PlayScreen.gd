6c00df06875cc683b2df1879df3eaef0420f6e37
Pingu <sunha2300@gmail.com>
2024-05-07 12:01:15 +0900

    rename: 함수 이름, 변수 이름 개발 문서 맞춰 수정

--- PlayScreen/PlayScreen.gd
+++ PlayScreen/PlayScreen.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var spawn_margin = 700
+
 var EnemyPresetScene = preload("res://PlayScreen/EnemyPreset.tscn")
 var GunPresetScene = preload("GunPreset.tscn")
 var mouse_position: Vector2
@@ -26,34 +26,33 @@ func _input(event):
 				$PauseMenu/CanvasLayer.hide()
 			pause_screen()
 
-func spawn_enemy():
-
-	# Randomly position the enemy first before adding to the scene
-	var player_position = $Player.position
-	var spawn_position = Vector2()
+func spawnEnemy():
+	
+	var SpawnMargin = 700
+	var PlayerPosition = $Player.position
+	var SpawnPosition = Vector2()
 	var EnemyPreset = EnemyPresetScene.instantiate()
 	
 	var enemy = [EnemyPreset]
 	
 	if randf() < 0.25:  # Spawn either to the left or right
-		spawn_position.x = player_position.x - spawn_margin
-		spawn_position.y = randf_range(-spawn_margin, spawn_margin)
+		SpawnPosition.x = PlayerPosition.x - SpawnMargin
+		SpawnPosition.y = randf_range(-SpawnMargin, SpawnMargin)
 	else: 
 		if randf() < 0.5:
-			spawn_position.x = player_position.x + spawn_margin
-			spawn_position.y = randf_range(-spawn_margin, spawn_margin)
+			SpawnPosition.x = PlayerPosition.x + SpawnMargin
+			SpawnPosition.y = randf_range(-SpawnMargin, SpawnMargin)
 		else:
 			if randf() < 0.75:
-				spawn_position.x = randf_range(-spawn_margin, spawn_margin)
-				spawn_position.y = player_position.y - spawn_margin
+				SpawnPosition.x = randf_range(-SpawnMargin, SpawnMargin)
+				SpawnPosition.y = PlayerPosition.y - SpawnMargin
 			else:
-				spawn_position.x = randf_range(-spawn_margin, spawn_margin)
-				spawn_position.y = player_position.y + spawn_margin
+				SpawnPosition.x = randf_range(-SpawnMargin, SpawnMargin)
+				SpawnPosition.y = PlayerPosition.y + SpawnMargin
 	
 	
 	var new_enemy = enemy[0]
-	new_enemy.position = spawn_position
-	
+	new_enemy.position = SpawnPosition
 	$Enemies.add_child(new_enemy)
 
 func pause_screen():
@@ -62,7 +61,7 @@ func pause_screen():
 
 
 func _on_enemy_spawn_timer_timeout():
-	spawn_enemy()
+	spawnEnemy()
 	$EnemySpawnTimer.start()
 
 
